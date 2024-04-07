bb5315b88ec27678f75e3ddc27dd73786f984156
vaanskii <giorgivanadze03@gmail.com>
2024-04-07 07:29:51 +0400

    Added Filtering Functionality. - Implemented filtering functionality in the new Vue component . - Users can now filter cars by manufacturer, car model, fuel type, location, year range, and price range. - Added dynamic options for manufacturer and car model based on user selection. - Included methods to reset all filtering fields for user convenience. - Styled the filtering form for improved visual clarity and user experience.

--- automarket_frontend/src/components/Filter.vue
+++ automarket_frontend/src/components/Filter.vue
@@ -2,68 +2,106 @@
     <div class="m-10 w-screen max-w-screen-md md:mt-24 mt-56">
       <div class="flex flex-col">
         <div class="rounded-xl border border-gray-200 bg-white p-6 shadow-lg">
-          <form id="opa">
+          <form id="opa" @submit.prevent="handleSubmit">
             <div class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-              <div class="flex flex-col text-black">
-                <label for="date" class="text-sm font-medium text-stone-600 uppercase">MANUFACTURER</label>
-                <select v-model="selectedBrand" class="mt-2 block w-full rounded-md border border-gray-100 bg-gray-100 px-2 py-2 shadow-sm outline-none">
-                    <option>BMW</option>
-                    <option>MERCEDES</option>
-                    <option>AUDI</option>
+              <!-- Manufacturer -->
+              <div class="flex flex-col">
+                <label for="manufacturer" class="text-sm font-medium text-stone-600 uppercase">Manufacturer</label>
+                <select v-model="selectedBrand" id="manufacturer" class="mt-2 text-black block w-full rounded-md border border-gray-100 bg-gray-100 px-2 py-2 shadow-sm outline-none">
+                  <option>BMW</option>
+                  <option>MERCEDES</option>
+                  <option>AUDI</option>
                 </select>
               </div>
+              <!-- Car -->
               <div class="flex flex-col">
-                <label for="date" class="text-sm font-medium text-stone-600 uppercase">car</label>
-                <select v-model="selectedCar" class="mt-2 block w-full cursor-pointer rounded-md border text-black border-gray-100 bg-gray-100 px-2 py-2 shadow-sm outline-none">
+                <label for="car" class="text-sm font-medium text-stone-600 uppercase">Car</label>
+                <select v-model="selectedCar" id="car" class="mt-2 block w-full cursor-pointer rounded-md border text-black border-gray-100 bg-gray-100 px-2 py-2 shadow-sm outline-none">
                   <option v-for="(car, index) in filteredCars" :key="index">{{ car }}</option>
                 </select>
               </div>
+              <!-- Gasoline -->
               <div class="flex flex-col">
-                <label for="date" class="text-sm font-medium text-stone-600 uppercase">Location</label>
-                <select class="mt-2 block w-full cursor-pointer rounded-md border text-black border-gray-100 bg-gray-100 px-2 py-2 shadow-sm outline-none">
-                    <option value="">TBILISI</option>
-                    <option value="">BATUMI</option>
-                    <option value="">KUTAISI</option>
+                <label for="fuel" class="text-sm font-medium text-stone-600 uppercase">Fuel</label>
+                <select v-model="selectedFuel" id="fuel" class="mt-2 text-black block w-full rounded-md border border-gray-100 bg-gray-100 px-2 py-2 shadow-sm outline-none">
+                    <option>Diesel</option>
+                    <option>Gasoline</option>
+                    <option>Gas</option>
+                </select>
+                </div>
+              <!-- Location -->
+              <div class="flex flex-col">
+                <label for="location" class="text-sm font-medium text-stone-600 uppercase">Location</label>
+                <select id="location" class="mt-2 block w-full cursor-pointer rounded-md border text-black border-gray-100 bg-gray-100 px-2 py-2 shadow-sm outline-none">
+                  <option value="">TBILISI</option>
+                  <option value="">BATUMI</option>
+                  <option value="">KUTAISI</option>
                 </select>
               </div>
+              <!-- Year Range -->
               <div class="flex flex-col">
-                <label for="date" class="text-sm font-medium text-stone-600 uppercase">year</label>
-                <input type="date" id="date" class="mt-2 block w-full cursor-pointer text-black rounded-md border border-gray-100 bg-gray-100 px-2 py-2 shadow-sm outline-none" />
+                <label for="startYear" class="text-sm font-medium text-stone-600 uppercase">Start Year</label>
+                <input placeholder="FROM" type="number" id="startYear" v-model="startYear" class="mt-2 block w-full cursor-pointer text-black rounded-md border border-gray-100 bg-gray-100 px-2 py-2 shadow-sm outline-none" />
+              </div>
+              <div class="flex flex-col">
+                <label for="endYear" class="text-sm font-medium text-stone-600 uppercase">End Year</label>
+                <input type="number" id="endYear" v-model="endYear" placeholder="TO" class="mt-2 block w-full cursor-pointer text-black rounded-md border border-gray-100 bg-gray-100 px-2 py-2 shadow-sm outline-none" />
+              </div>
+
+              <div class="flex flex-col">
+                <label for="startPrice" class="text-sm font-medium text-stone-600 uppercase">start price</label>
+                <input placeholder="FROM" type="number" id="startPrice" v-model="startPrice" class="mt-2 block w-full cursor-pointer text-black rounded-md border border-gray-100 bg-gray-100 px-2 py-2 shadow-sm outline-none" />
+              </div>
+              <div class="flex flex-col">
+                <label for="endPrice" class="text-sm font-medium text-stone-600 uppercase">End price</label>
+                <input type="number" id="endPrice" v-model="endPrice" placeholder="TO" class="mt-2 block w-full cursor-pointer text-black rounded-md border border-gray-100 bg-gray-100 px-2 py-2 shadow-sm outline-none" />
               </div>
             </div>
-  
+            
             <div class="mt-6 grid w-full grid-cols-2 justify-end space-x-4 md:flex">
-              <button class="rounded-lg bg-gray-200 px-8 py-2 font-medium text-gray-700 outline-none hover:opacity-80 focus:ring">Reset</button>
-              <button class="rounded-lg bg-blue-600 px-8 py-2 font-medium text-white outline-none hover:opacity-80 focus:ring">Search</button>
+              <button type="button" @click="resetFields" class="rounded-lg bg-gray-200 px-8 py-2 font-medium text-gray-700 outline-none hover:opacity-80 focus:ring">Reset</button>
+              <button type="submit" class="rounded-lg bg-blue-600 px-8 py-2 font-medium text-white outline-none hover:opacity-80 focus:ring">Search</button>
             </div>
           </form>
         </div>
       </div>
     </div>
-  </template>
+</template>
   
-  <script>
-  export default {
-    data() {
-      return {
-        selectedBrand: '',
-        selectedCar: '',
-        cars: {
-          BMW: ['BMW - 5 SERIES', 'BMW - 3 SERIES', 'BMW - X5'],
-          MERCEDES: ['MERCEDES - E CLASS', 'MERCEDES - C CLASS', 'MERCEDES - GLE'],
-          AUDI: ['AUDI - A4', 'AUDI - Q5', 'AUDI - TT']
-        }
-      };
-    },
-    computed: {
-      filteredCars() {
-        return this.selectedBrand ? this.cars[this.selectedBrand] : [];
-      }
+<script>
+export default {
+data() {
+    return {
+    selectedBrand: '',
+    selectedCar: '',
+    selectedFuel: '',
+    startYear: '',
+    endYear: '',
+    startPrice: '',
+    endPrice: '',
+    cars: {
+        BMW: ['BMW - 5 SERIES', 'BMW - 3 SERIES', 'BMW - X5'],
+        MERCEDES: ['MERCEDES - E CLASS', 'MERCEDES - C CLASS', 'MERCEDES - GLE'],
+        AUDI: ['AUDI - A4', 'AUDI - Q5', 'AUDI - TT']
     }
-  };
-  </script>
-  
-
-<style scoped>
-
-</style>
+    };
+},
+computed: {
+    filteredCars() {
+    return this.selectedBrand ? this.cars[this.selectedBrand] : [];
+    }
+},
+methods: {
+    resetFields() {
+    // Reset all fields
+    this.selectedBrand = '',
+    this.selectedCar = '',
+    this.selectedFuel = '',
+    this.startYear = '',
+    this.endYear = '',
+    this.startPrice= '',
+    this.endPrice= ''
+    }
+}
+};
+</script>
