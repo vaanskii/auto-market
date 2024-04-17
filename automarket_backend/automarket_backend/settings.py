12fc858d92d2836ef5a72ae13c035ea7bc3d2478
vaanskii <giorgivanadze03@gmail.com>
2024-04-17 19:51:46 +0400

    implement postgres in project.

--- automarket_backend/automarket_backend/settings.py
+++ automarket_backend/automarket_backend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'automarket_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'automarket_database',
+        'USER': 'postgres',
+        'PASSWORD': 'Vanski159',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
