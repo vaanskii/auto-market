06e439df93e40d411c7079f1a5e1aec108ccc9ae
vaanskii <giorgivanadze03@gmail.com>
2024-04-18 04:01:10 +0400

    Implement signup API endpoint - Added signup API view in views.py to handle user registration. - Imported necessary modules such as api_view, SignupForm, JsonResponse, and status. - Defined the signup function to handle POST requests. - Initialized the SignupForm with request data. - Validated the form data and saved the user if valid. - Returned appropriate JSON responses for success and error cases.

--- automarket_backend/account/views.py
+++ automarket_backend/account/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .forms import SignupForm
+from django.http import JsonResponse
+from rest_framework import status
 
-# Create your views here.
+@api_view(['POST'])
+def signup(request):
+    data = request.data
+    message = 'success'
+
+    form = SignupForm({
+        'email': data.get('email'),
+        'name': data.get('name'),
+        'mobile_number': data.get('mobile_number'),
+        'password1': data.get('password1'),
+        'password2': data.get('password2')
+    })
+
+    if form.is_valid():
+        user = form.save()
+        user.save()
+        return JsonResponse({'message': message}, status=status.HTTP_201_CREATED)
+    else:
+        return JsonResponse({'message': 'error', 'errors': form.errors}, status=status.HTTP_400_BAD_REQUEST)
