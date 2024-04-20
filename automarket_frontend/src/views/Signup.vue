1a482f17ce9aaf29c0641f4988b0833c53aacd2b
vaanskii <giorgivanadze03@gmail.com>
2024-04-20 16:19:46 +0400

    integrate changes on login vue and signup vue.

--- automarket_frontend/src/views/Signup.vue
+++ automarket_frontend/src/views/Signup.vue
@@ -97,32 +97,12 @@
             </div>
             <div>
                 <button 
-                    class="btn2 w-full px-10 py-5 text-lg relative transition-colors border border-black rounded-md uppercase font-semibold tracking-wider leading-none overflow-hidden hover:text-white" 
+                    class="btn2 w-full mt-10 px-10 py-5 text-lg relative transition-colors border border-black rounded-md uppercase font-semibold tracking-wider leading-none overflow-hidden hover:text-white" 
                     type="submit"
                     >
                     <span class="absolute inset-0 bg-[#222]"></span>
                     <span class="absolute inset-0 flex justify-center items-center font-bold">{{ $t('signup.sign-up') }}</span>
                 </button>
-            </div>
-            <div class="flex flex-col space-y-5">
-              <span class="flex items-center justify-center space-x-2">
-                <span class="h-px bg-gray-400 w-14"></span>
-                <span class="font-normal text-gray-500">or connect with</span>
-                <span class="h-px bg-gray-400 w-14"></span>
-              </span>
-              <div class="flex flex-col space-y-4">
-                <a
-                  href="#"
-                  class="flex items-center justify-center px-4 py-2 space-x-2 transition-colors duration-300 border border-gray-800 rounded-md group hover:bg-black focus:outline-none"
-                >
-                  <span>
-                    <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48">
-                        <path fill="#fbc02d" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12	s5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24s8.955,20,20,20	s20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path><path fill="#e53935" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039	l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path><path fill="#4caf50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36	c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"></path><path fill="#1565c0" d="M43.611,20.083L43.595,20L42,20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571	c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"></path>
-                    </svg>
-                  </span>
-                  <span class="text-sm font-medium text-black group-hover:text-white">Google</span>
-                </a>
-              </div>
             </div>
             <template v-if="errors.length > 0">
                 <div class="bg-red-300 text-white rounded-lg p-6">
