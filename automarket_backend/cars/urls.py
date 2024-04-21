7bbbcce570b4a65aff7abfc70646f35a92b50e9f
vaanskii <giorgivanadze03@gmail.com>
2024-04-21 15:29:55 +0400

    Added serializers for account and cars apps - Added serializers.cpython-312.pyc for account app. - Added serializers.cpython-312.pyc for cars app. - Modified urls.cpython-312.pyc and views.cpython-312.pyc in cars app. - Updated urls.py in cars app with new routes.

--- automarket_backend/cars/urls.py
+++ automarket_backend/cars/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('create/', views.create_cars, name='create_cars')
 ]
--- automarket_backend/cars/views.py
+++ automarket_backend/cars/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from django.http import JsonResponse
+from .forms import ImageModelForm, CarForm
+from .serializers import CarSerializer
 
-# Create your views here.
+
+@api_view(['POST'])
+def create_cars(request):
+    form = CarForm(request.POST)
+    image = None
+    image_form = ImageModelForm(request.POST, request.FILES)
+
+    if image_form.is_valid():
+        image = image_form.save(commit=False)
+        image.created_by = request.user
+        image.save()
+
+    if form.is_valid():
+        cars = form.save(commit=False)
+        cars.created_by = request.user
+        cars.save()
+
+        if image:
+            cars.images.add(image)
+
+        user = request.user
+        user.save()
+
+        serializer = CarSerializer(cars)
+
+        return JsonResponse(serializer.data, safe=False)
+    else:
+        return JsonResponse({'error': 'add something here later!...'})
