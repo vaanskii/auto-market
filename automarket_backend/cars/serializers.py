5ec1e47025c7a50a1fcd1b1772d94a4b55497b23
vaanskii <giorgivanadze03@gmail.com>
2024-04-21 17:43:36 +0400

    Added ChoiceSerializer to serializers.py - Added ChoiceSerializer to handle serialization for various choice fields such as manufacturer, car_model, types, categories, etc.

--- automarket_backend/cars/serializers.py
+++ automarket_backend/cars/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import ImageModel, Car
+from .choices import (MANUFACTURER_CHOICES, CAR_MODELS, TYPE, CATEGORIES, LOCATION, FUEL_TYPES, TRANSMISSION_TYPES, CYLINDERS, DOORS, 
+                      DRIVE_WHEELS, WHEEL, AIRBAG_OPTIONS, CAR_COLORS, INTERIOR_MATERIAL, INTERIOR_COLORS)
 from account.serializers import UserSerializer
 
 class ImageModelSerializer(serializers.ModelSerializer):
@@ -15,3 +17,21 @@ class CarSerializer(serializers.ModelSerializer):
         fields = ('id','manufacturer', 'car_model', 'types', 'categories', 'price', 'year', 'location', 'engine_volume',
                     'milage', 'fuel_type', 'transmission', 'cylinders', 'doors', 'drive_wheels', 'wheel', 'airbags',
                     'car_colors', 'interior', 'interior_color', 'description', 'images')
+        
+class ChoiceSerializer(serializers.Serializer):
+    manufacturer = serializers.ChoiceField(choices=MANUFACTURER_CHOICES)
+    car_model = serializers.ChoiceField(choices=CAR_MODELS)
+    types = serializers.ChoiceField(choices=TYPE)
+    categories = serializers.ChoiceField(choices=CATEGORIES)
+    location = serializers.ChoiceField(choices=LOCATION)
+    fuel_type = serializers.ChoiceField(choices=FUEL_TYPES)
+    transmission = serializers.ChoiceField(choices=TRANSMISSION_TYPES)
+    cylinders = serializers.ChoiceField(choices=CYLINDERS)
+    doors = serializers.ChoiceField(choices=DOORS)
+    drive_wheels = serializers.ChoiceField(choices=DRIVE_WHEELS)
+    wheel = serializers.ChoiceField(choices=WHEEL)
+    airbags = serializers.ChoiceField(choices=AIRBAG_OPTIONS) 
+    car_colors = serializers.ChoiceField(choices=CAR_COLORS) 
+    interior = serializers.ChoiceField(choices=INTERIOR_MATERIAL) 
+    interior_color = serializers.ChoiceField(choices=INTERIOR_COLORS)
+    interior_color = serializers.ChoiceField(choices=INTERIOR_COLORS)
