6c0468cd3e2fb31df9c0405e2186d6132d7aeae5
vaanskii <giorgivanadze03@gmail.com>
2024-04-21 17:45:40 +0400

    Updated urls.py - Added a new endpoint for retrieving choices data in JSON format using ChoicesAPIView.

--- automarket_backend/cars/urls.py
+++ automarket_backend/cars/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import ChoicesAPIView
 
 urlpatterns = [
-    path('create/', views.create_cars, name='create_cars')
+    path('create/', views.create_cars, name='create-cars'),
+    path('choices/', ChoicesAPIView.as_view(), name='api-choices')
 ]
