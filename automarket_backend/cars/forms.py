eee2b3f3330b7064dfddef34ba5d294a0b44aa1e
vaanskii <giorgivanadze03@gmail.com>
2024-04-21 14:24:38 +0400

    Added forms and serializers for cars app - Added ImageModelForm and CarForm for handling image uploads and car data input. - Implemented ImageModelSerializer and CarSerializer for API serialization of ImageModel and Car models, respectively. - Imported necessary modules and models. - Utilized UserSerializer from the account app for the created_by field in CarSerializer.

--- automarket_backend/cars/forms.py
+++ automarket_backend/cars/forms.py
@@ -1,1 +1,13 @@
-from django import forms
+from django.forms import ModelForm
+from .models import ImageModel, Car
+
+class ImageModelForm(ModelForm):
+    class Meta:
+        model = ImageModel
+        fields = ('image',)
+
+class CarForm(ModelForm):
+    model = Car
+    fields = ('manufacturer', 'car_model', 'types', 'categories', 'price', 'year', 'location', 'engine_volume',
+              'milage', 'fuel_type', 'transmission', 'cylinders', 'doors', 'drive_wheels', 'wheel', 'airbags',
+                'car_colors', 'interior', 'interior_color', 'description')
--- automarket_backend/cars/serializers.py
+++ automarket_backend/cars/serializers.py
@@ -1,1 +1,17 @@
 from rest_framework import serializers
+from .models import ImageModel, Car
+from account.serializers import UserSerializer
+
+class ImageModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ImageModel
+        fields = ('id', 'get_image',)
+
+class CarSerializer(serializers.ModelSerializer):
+    created_by = UserSerializer(read_only=True)
+    images = ImageModelSerializer(read_only=True, many=True)
+    class Meta:
+        model = Car
+        fields = ('id','manufacturer', 'car_model', 'types', 'categories', 'price', 'year', 'location', 'engine_volume',
+                    'milage', 'fuel_type', 'transmission', 'cylinders', 'doors', 'drive_wheels', 'wheel', 'airbags',
+                    'car_colors', 'interior', 'interior_color', 'description', 'images')
