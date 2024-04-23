ac7202ff367561fa1904d1151a7a1eafc434015c
vaanskii <giorgivanadze03@gmail.com>
2024-04-24 01:02:06 +0400

    Added styles to SearchView component - Enhanced the visual presentation of search results in SearchView.vue by adding styles. - Adjusted the layout and appearance of the search results to improve readability and user experience.

--- automarket_frontend/src/views/SearchView.vue
+++ automarket_frontend/src/views/SearchView.vue
@@ -1,13 +1,22 @@
 <template>
-  <div class="pt-14">
-    <div v-if="cars.length" v-for="car in cars" :key="car.id">
-      <div class="bg-gray-300 mb-10">
-        <h1>{{ car.location }}</h1>
-        <router-link :to="Trans.i18nRoute({ name: 'cardetail', params: {'id': car.id} })">
-          <img v-if="car.main_image" :src="car.main_image.image_url" class="w-96 cursor-pointer">
-          <h1 class="cursor-pointer"><span class="font-bold">{{ car.manufacturer }}</span> - {{ car.car_model }} </h1>
-        </router-link>
-        <p>{{ car.price }} $</p>
+  <div class="pt-14 flex justify-evenly w-full">
+    <div v-if="cars.length">
+      <h1 class="uppercase font-sans font-extralight lg:text-4xl md:text-4xl text-2xl mt-16 text-center">search results</h1>
+      <div class="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 gap-x-16 gap-y-20 mt-16">
+        <div v-for="car in cars" :key="car.id" class="max-w-[280px] h-[350px] shadow-2xl z-1 w-full flex flex-col justify-start bg-[#E6E6E6] rounded-xl reveal-car">
+          <router-link :to="Trans.i18nRoute({ name: 'cardetail', params: {'id': car.id} })">
+            <img :v-if="car.main_image" :src="car.main_image.image_url" class="w-full h-44 rounded-t-xl transition-transform transform hover:scale-105 cursor-pointer" alt="">
+          </router-link>
+          <hr class="h-px bg-black border-0">
+          <span class="uppercase text-[10px] mt-2 ml-2 mb-2">{{ car.location }}</span>
+          <span class="whitespace-nowrap overflow-hidden overflow-ellipsis cursor-pointer ml-2 uppercase text-lg text-gray-900 hover:text-red-900 font-semibold">{{ car.manufacturer }} - {{ car.car_model }}</span>
+          <span class="uppercase font-bold mt-4 ml-2 text-gray-900">{{ car.price }} $</span>
+          <hr class="h-px mt-7 bg-black border-0">
+          <div class="flex flex-row mt-2 ml-2">
+            <span class="uppercase text-[10px] p-2 bg-black text-white rounded-full px-3">{{ car.types }}</span>
+            <span class="uppercase text-[10px] p-2 bg-black text-white rounded-full px-3 ml-2">{{ car.fuel_type }}</span>
+          </div>
+        </div>
       </div>
     </div>
     <div v-else class="flex justify-center items-center flex-col">
@@ -16,7 +25,6 @@
     </div>
   </div>
 </template>
-
 
 <script>
 import { RouterLink } from 'vue-router'
@@ -45,6 +53,7 @@ fetchSearchResults() {
   const params = this.$route.query
   axios.get('/api/filters/', { params })
     .then(response => {
+      console.log(response.data)
       this.cars = response.data
     })
     .catch(error => {
