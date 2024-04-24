8f3fe91214582f74252475816da6942e5c2a2cf7
vaanskii <giorgivanadze03@gmail.com>
2024-04-24 18:11:26 +0400

    Modified UserAPIView and SignupAPIView to use APIView instead of @api_views - Updated the UserAPIView and SignupAPIView classes to inherit from APIView instead of @api_views.

--- automarket_backend/account/urls.py
+++ automarket_backend/account/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from . import views
+from .views import *
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('signup/', views.signup, name='signup'),
-    path('user/', views.user, name='user'),
+    path('signup/', SignupAPIView.as_view(), name='signup'),
+    path('user/', UserAPIView.as_view(), name='user')
 ]
--- automarket_backend/account/views.py
+++ automarket_backend/account/views.py
@@ -1,50 +1,49 @@
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
 from .forms import SignupForm
-from django.http import JsonResponse
 
-@api_view(['GET'])
-def user(request):
-    if hasattr(request.user, 'mobile_number'):
-        mobile_number = str(request.user.mobile_number)
-    else:
-        mobile_number = None
+class UserAPIView(APIView):
+    def get(self, request):
+        if hasattr(request.user, 'mobile_number'):
+            mobile_number = str(request.user.mobile_number)
+        else:
+            mobile_number = None
 
-    return JsonResponse({
-        'id': request.user.id,
-        'name': request.user.name,
-        'email': request.user.email,
-        'mobile_number': mobile_number,
-        'country_code': request.user.country_code
-    })
+        data = {
+            'id': request.user.id,
+            'name': request.user.name,
+            'email': request.user.email,
+            'mobile_number': mobile_number,
+            'country_code': request.user.country_code
+        }
+
+        return Response(data, status=status.HTTP_200_OK)
 
 
-@api_view(['POST'])
-@authentication_classes([])
-@permission_classes([])
-def signup(request):
-    data = request.data
-    message = 'success'
+class SignupAPIView(APIView):
+    def post(self, request):
+        data = request.data
+        message = 'success'
 
-    # Extract country code and mobile number from the request
-    country_code = data.get('country_code')
-    mobile_number = f"{country_code}{data.get('mobile_number')}"
+        country_code = data.get('country_code')
+        mobile_number = f"{country_code}{data.get('mobile_number')}"
 
-    # Create the form with the extracted country code and mobile number
-    form = SignupForm({
-        'email': data.get('email'),
-        'name': data.get('name'),
-        'mobile_number': mobile_number,
-        'password1': data.get('password1'),
-        'password2': data.get('password2'),
-        'country_code': country_code
-    })
+        form = SignupForm({
+            'email': data.get('email'),
+            'name': data.get('name'),
+            'mobile_number': mobile_number,
+            'password1': data.get('password1'),
+            'password2': data.get('password2'),
+            'country_code': country_code
+        })
 
-    if form.is_valid():
-        user = form.save(commit=False)
-        user.country_code = country_code
-        user.save()
-    else:
-        message = form.errors.as_json()
-    
-    return JsonResponse({'message': message}, safe=False)
-
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.country_code = country_code
+            user.save()
+        else:
+            message = form.errors.as_json()
+        
+        return Response({'message': message}, status=status.HTTP_200_OK)
