5af3be37b5f1842b03aba8d86fbc7aebe839335f
vaanskii <giorgivanadze03@gmail.com>
2024-04-25 13:24:02 +0400

    Updated SettingsView.vue template - Refactored the layout of the SettingsView.vue template to improve readability and organization. - Added a link to change the password below the introductory paragraph for easy navigation. - Updated the form layout in the right panel to follow a more conventional style with labeled input fields. - Improved the styling of the form inputs for better user experience and accessibility.             - Implemented error handling to display validation errors if any required field is missing or if the API response indicates an error. - Updated the button styling to include a background color change on hover for better visual feedback. - Added custom styling for the shadow effect on the right panel for a more modern appearance.

--- automarket_frontend/src/views/SettingsView.vue
+++ automarket_frontend/src/views/SettingsView.vue
@@ -1,6 +1,29 @@
 <template>
-  <div class="pt-14">
-      <form v-on:submit.prevent="submitForm" action="#" class="flex flex-col space-y-5">
+  <div class="md:pt-0 pt-14">
+      <div class="flex items-center min-h-screen p-4 lg:justify-center md:mt-0 mt-20">
+    <div
+      class="flex flex-col overflow-hidden bg-white rounded-md shadow-4xl max md:flex-row md:flex-1 lg:max-w-screen-md"
+    >
+      <div
+        class="p-4 py-6 text-white bg-[#222] md:w-80 md:flex-shrink-0 md:flex md:flex-col md:items-center md:justify-evenly"
+      >
+        <div class="my-3 text-4xl font-bold tracking-wider text-center">
+          <p class="uppercase">autohub</p>
+        </div>
+        <p class="mt-6 font-normal text-center text-gray-300 md:mt-0">
+          {{ $t('changeinfo.paragraph') }}
+        </p>
+        <p class="flex flex-col items-center justify-center mt-10 text-center">
+            <span>{{ $t('changeinfo.password') }}</span>
+            <router-link :to="Trans.i18nRoute({ name: 'password', params: {'id': userStore.user.id} })" class="underline mt-px">{{ $t('changeinfo.getstart') }}</router-link>
+          </p>
+        <p class="mt-6 text-sm text-center text-gray-300">
+          Read our <a href="#" class="underline">terms</a> and <a href="#" class="underline">conditions</a>
+        </p>
+      </div>
+      <div class="p-5 bg-[#E6E6E6] md:flex-1">
+        <h3 class="my-4 text-2xl font-semibold text-gray-700 uppercase">Change user info</h3>
+        <form v-on:submit.prevent="submitForm" action="#" class="flex flex-col space-y-5">
           <div class="flex flex-col space-y-1">
             <label for="email" class="text-sm font-semibold text-gray-500">Email address</label>
             <input
@@ -59,6 +82,9 @@
               </div>
           </template>
         </form>
+      </div>
+    </div>
+  </div>
   </div>
 </template>
 
@@ -66,11 +92,13 @@
 import axios from 'axios'
 import { useUserStore } from '@/stores/user'
 import { RouterLink } from 'vue-router'
+import Trans from '@/i18n/translation'
 export default{
 setup() {
   const userStore = useUserStore()
   return {
-      userStore
+      userStore,
+      Trans
   }
 },
 data() {
@@ -105,22 +133,41 @@ if(this.errors.length === 0){
   }
 
   axios.post('/api/editprofile/', payload)
-    .then(response => {
-      console.log('Profile updated successfully', response.data)
-      this.userStore.setUserInfo({
-        id: this.userStore.user.id,
-        name: this.form.name,
-        email: this.form.email,
-        mobile_number: fullMobileNumber,
-        country_code: this.form.country_code
+      .then(response => {
+        if(response.data.message === 'profile uploaded successfully'){
+          console.log('Profile updated successfully', response.data)
+          this.userStore.setUserInfo({
+            id: this.userStore.user.id,
+            name: this.form.name,
+            email: this.form.email,
+            mobile_number: fullMobileNumber,
+            country_code: this.form.country_code
+          })
+          this.$router.back()
+        }else{
+          this.errors.push(response.data.message)
+        }
       })
-      this.$router.back()
-    })
-    .catch(error => {
-      console.log('An error occurred: ', error)
-    })
-}
-} 
+      .catch(error => {
+        console.log('An error occurred: ', error)
+      })
+    }
+  } 
 }
 }
 </script>
+
+<style scoped>
+.shadow-4xl {
+    --tw-shadow: 0 50px 100px -20px rgba(0, 0, 0, 0.4);
+    --tw-shadow-colored: 0 50px 100px -20px var(--tw-shadow-color);
+    box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
+}
+.btn2 span:first-child{
+  transform: translateX(-101%);
+  transition: transform .3s ease-in
+}
+.btn2:hover span{
+  transform: translateX(0)
+}
+</style>
