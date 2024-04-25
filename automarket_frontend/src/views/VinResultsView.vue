3dee0f9212b03d5fa984b3cd6f7f784606a96def
vaanskii <giorgivanadze03@gmail.com>
2024-04-25 22:36:15 +0400

    Added 'allow_null' option to VIN data serializer fields - Modified the VinDataSerializer in the backend to include the 'allow_null' option for each field, allowing null values to be accepted. - In the VinResultsView component in the frontend:   - Implemented logic to display 'No information provided' when a value is null or undefined in the VIN data.   - Modified the decoding process to replace null or undefined values with 'No information provided' in the displayed VIN data.

--- automarket_frontend/src/views/VinResultsView.vue
+++ automarket_frontend/src/views/VinResultsView.vue
@@ -1,50 +1,52 @@
 <template>
-    <div class="vin-decoder pt-14">
-      <div v-if="vinData">
-        <h3>Vehicle Information</h3>
-        <ul>
-          <li v-for="(value, key) in vinData" :key="key">
-            <strong>{{ key.split('_').join(' ') }}:</strong> {{ value }}
-          </li>
-        </ul>
-      </div>
-      <div v-else>
-        <p>Loading VIN data...</p>
-      </div>
+  <div class="vin-decoder pt-14">
+    <div v-if="vinData">
+      <h3>Vehicle Information</h3>
+      <ul>
+        <li v-for="(value, key) in vinData" :key="key">
+          <strong>{{ key.split('_').join(' ') }}:</strong> {{ value || 'no-info' }}
+        </li>
+      </ul>
     </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        vinData: null,
-        error: null
-      };
-    },
-    created() {
-      this.decodeVin();
-    },
-    methods: {
-      decodeVin() {
-        const vin = this.$route.query.q;
-        if (vin) {
-          axios.get(`/api/vin/?vin=${vin}`)
-            .then(response => {
-              this.vinData = response.data;
-              this.error = null;
-            })
-            .catch(error => {
-              console.error('Error decoding VIN:', error);
-              this.vinData = null;
-              this.error = 'Failed to decode VIN. Please try again.';
-            });
-        } else {
-          this.error = 'No VIN provided.';
-        }
+    <div v-else>
+      <p>Loading VIN data...</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      vinData: null,
+      error: null
+    };
+  },
+  created() {
+    this.decodeVin();
+  },
+  methods: {
+    decodeVin() {
+      const vin = this.$route.query.q;
+      if (vin) {
+        axios.get(`/api/vin/?vin=${vin}`)
+          .then(response => {
+            this.vinData = Object.fromEntries(
+              Object.entries(response.data).map(([key, value]) => [key, value || 'No information provided'])
+            );
+            this.error = null;
+          })
+          .catch(error => {
+            console.error('Error decoding VIN:', error);
+            this.vinData = null;
+            this.error = 'Failed to decode VIN. Please try again.';
+          });
+      } else {
+        this.error = 'No VIN provided.';
       }
     }
-  };
-  </script>  
+  }
+};
+</script>
