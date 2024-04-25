f350cdf672ae10d3c6b515b63450fa62515e405e
vaanskii <giorgivanadze03@gmail.com>
2024-04-26 00:19:14 +0400

    Integrate styles for vinresult page. - Integrate tables for values and keys in vinresult page. - Added styles for tables and make it responsive. - Style div for loading animation.

--- automarket_frontend/src/views/VinResultsView.vue
+++ automarket_frontend/src/views/VinResultsView.vue
@@ -1,23 +1,35 @@
 <template>
-  <div class="vin-decoder pt-14">
-    <div v-if="vinData">
-      <h3>Vehicle Information</h3>
-      <ul>
-        <li v-for="(value, key) in vinData" :key="key">
-          <strong>{{ key.split('_').join(' ') }}:</strong> {{ value || 'no-info' }}
-        </li>
-      </ul>
+  <div class="pt-14">
+    <div v-if="vinData" class="flex mt-10 justify-center overflow-x-auto items-center flex-col">
+      <h3 class="uppercase font-bold md:text-4xl text-xl mt-10">{{ $t('vin.information') }}</h3>
+      <div class="overflow-x-auto mt-10 shadow-2xl bg-zinc-200">
+        <table class="min-w-full shadow-2xl table-auto border-collapse border border-gray-200">
+          <tbody>
+            <tr v-for="(value, key) in vinData" :key="key" class="hover:bg-gray-100">
+              <td class="border md:max-w-full max-w-[15rem] truncate md:text-lg text-xs uppercase border-black px-4 py-2">{{ key }}</td>
+              <td class="border md:text-lg text-xs uppercase border-black px-4 py-2">{{ value || 'no-info' }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-    <div v-else>
-      <p>Loading VIN data...</p>
+    <div v-else class="h-screen">
+      <p class="text-center text-4xl mt-80">{{ $t('vin.loading') }}</p>
+      <div id="loading-bar-spinner" class="spinner"><div class="spinner-icon"></div></div>
     </div>
   </div>
 </template>
 
+
 <script>
 import axios from 'axios';
-
+import Trans from '@/i18n/translation';
 export default {
+  setup() {
+    return {
+      Trans
+    }
+  },
   data() {
     return {
       vinData: null,
@@ -34,7 +46,7 @@ export default {
         axios.get(`/api/vin/?vin=${vin}`)
           .then(response => {
             this.vinData = Object.fromEntries(
-              Object.entries(response.data).map(([key, value]) => [key, value || 'No information provided'])
+              Object.entries(response.data).map(([key, value]) => [key, value || 'NO INFORMATION PROVIDED'])
             );
             this.error = null;
           })
@@ -50,3 +62,30 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+#loading-bar-spinner.spinner {
+    left: 50%;
+    margin-left: -20px;
+    top: 50%;
+    margin-top: -20px;
+    position: absolute;
+    z-index: 19 !important;
+    animation: loading-bar-spinner 400ms linear infinite;
+}
+
+#loading-bar-spinner.spinner .spinner-icon {
+    width: 40px;
+    height: 40px;
+    border:  solid 4px transparent;
+    border-top-color:  black !important;
+    border-left-color: black !important;
+    border-radius: 50%;
+}
+
+@keyframes loading-bar-spinner {
+    0%   { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+}
+
+</style>
