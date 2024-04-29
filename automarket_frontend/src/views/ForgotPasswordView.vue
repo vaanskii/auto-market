0a8104c48137439731db4087e02c1f0a1b9a2ac3
vaanskii <giorgivanadze03@gmail.com>
2024-04-29 21:26:41 +0400

    Update styles in ForgotPasswordView and ResetPasswordView - Made style enhancements and updates in ForgotPasswordView.vue and ResetPasswordView.vue to improve the visual appearance and user experience of the pages.

--- automarket_frontend/src/views/ForgotPasswordView.vue
+++ automarket_frontend/src/views/ForgotPasswordView.vue
@@ -1,23 +1,54 @@
 <template>
-  <form class="pt-20" @submit.prevent="submit">
-    <h1 class="h3 mb-3 fw-normal">Please register</h1>
-
-    <input
-      autocomplete="email"
-      id="email"
-      v-model="data.email"
-      type="email"
-      class="form-control"
-      placeholder="Email"
-      required
-    />
-
-    <button class="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
-  </form>
+  <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
+      <div class="w-full p-6 bg-[#222] rounded-lg shadow dark:border md:mt-0 sm:max-w-md  sm:p-8">
+          <h1 class="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Forgot your password?
+          </h1>
+          <p class="font-light text-white">Don't fret! Just type in your email and we will send you a code to reset your password!</p>
+          <form  @submit.prevent="submit" class="mt-4 space-y-4 lg:mt-5 md:space-y-5">
+              <div>
+                  <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                  <input 
+                    v-model="data.email" 
+                    type="email" 
+                    name="email" 
+                    autocomplete="email"
+                    id="email" 
+                    class="bg-gray-50 border border-white placeholder:text-black text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " 
+                    placeholder="name@company.com" 
+                    required
+                  >
+              </div>
+              <div class="flex items-start">
+                  <div class="flex items-center h-5">
+                    <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">
+                  </div>
+                  <div class="ml-3 text-sm">
+                    <label for="terms" class="font-light text-gray-500 dark:text-gray-300">I accept the <a class="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
+                  </div>
+              </div>
+              <button 
+                    class="btn2 w-full mt-10 px-10 py-5 text-lg relative transition-colors border border-white rounded-md uppercase font-semibold tracking-wider leading-none overflow-hidden hover:text-white" 
+                    type="submit"
+                    >
+                    <span class="absolute inset-0 bg-white"></span>
+                    <span class="absolute inset-0 flex text-white justify-center hover:text-black items-center font-bold">Reset password</span>
+                </button>
+                <transition name="fade" mode="out-in">
+                <div v-if="submitSuccessfully" key="confirmation" id="toast-simple" class="flex mt-4  items-center w-full p-4 space-x-4 rtl:space-x-reverse text-black uppercase bg-green-300 rounded-lg shadow-2xl" role="alert">
+                    <svg class="w-8 h-8 text-black rotate-45" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m9 17 8 2L9 1 1 19l8-2Zm0 0V9"/>
+                    </svg>
+                    <p class="ps-4 text-sm font-normal">We've sent a confirmation message to your email. Please check your inbox.</p>
+                </div>
+              </transition>
+          </form>
+      </div>
+  </div>
 </template>
 
 <script>
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
 import axios from 'axios';
 
 export default {
@@ -26,16 +57,18 @@ export default {
     const data = reactive({
       email: '',
     });
+    const submitSuccessfully = ref(false);
 
     const submit = async () => {
       try {
-        // Make a POST request to your Django API endpoint
         await axios.post('/api/forgot/', data);
-
-        // Handle success (e.g., show a success message)
+        data.email = '',
+        submitSuccessfully.value = true
+        setTimeout(() => {
+          submitSuccessfully.value = false;
+        }, 5000);
         console.log('Password reset email sent successfully!');
       } catch (error) {
-        // Handle error (e.g., show an error message)
         console.error('Error sending password reset email:', error.message);
       }
     };
@@ -43,7 +76,26 @@ export default {
     return {
       data,
       submit,
+      submitSuccessfully
     };
   },
 };
 </script>
+
+<style>
+.btn2 span:first-child{
+  transform: translateX(-101%);
+  transition: transform .3s ease-in
+}
+.btn2:hover span{
+  transform: translateX(0)
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
--- automarket_frontend/src/views/ResetPasswordView.vue
+++ automarket_frontend/src/views/ResetPasswordView.vue
@@ -1,16 +1,59 @@
 <template>
-    <form class="pt-14" @submit.prevent="submit">
-      <h1 class="h3 mb-3 fw-normal">Please register</h1>
-
-      <input v-model="data.password" type="password" class="form-control" placeholder="Password" required>
-      <input v-model="data.password_confirm" type="password" class="form-control" placeholder="Password Confirm" required>
-  
-      <button class="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
-    </form>
-  </template>
+  <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
+      <div class="w-full p-6 bg-[#222] rounded-lg shadow dark:border md:mt-0 sm:max-w-md  sm:p-8">
+          <h1 class="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Forgot your password?
+          </h1>
+          <p class="font-light text-white">Don't fret! Just type in your email and we will send you a code to reset your password!</p>
+          <form  @submit.prevent="submit" class="mt-4 space-y-4 lg:mt-5 md:space-y-5">
+              <div>
+                  <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New Password</label>
+                  <input 
+                  v-model="data.password" 
+                    type="password" 
+                    name="password" 
+                    autocomplete="password"
+                    id="email" 
+                    class="bg-gray-50 border border-white placeholder:text-black text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " 
+                    placeholder="*********" 
+                    required
+                  >
+              </div>
+              <div>
+                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm New Password</label>
+                  <input 
+                  v-model="data.password_confirm" 
+                    type="password" 
+                    name="password" 
+                    autocomplete="password"
+                    id="email" 
+                    class="bg-gray-50 border border-white placeholder:text-black text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " 
+                    placeholder="*********" 
+                    required
+                  >
+              </div>
+              <button 
+                    class="btn2 w-full mt-10 px-10 py-5 text-lg relative transition-colors border border-white rounded-md uppercase font-semibold tracking-wider leading-none overflow-hidden hover:text-white" 
+                    type="submit"
+                    >
+                    <span class="absolute inset-0 bg-white"></span>
+                    <span class="absolute inset-0 flex text-white justify-center hover:text-black items-center font-bold">Confirm password</span>
+                </button>
+                <transition name="fade" mode="out-in">
+                <div v-if="submitSuccessfully" key="confirmation" id="toast-simple" class="flex mt-4  items-center w-full p-4 space-x-4 rtl:space-x-reverse text-black uppercase bg-green-300 rounded-lg shadow-2xl" role="alert">
+                    <svg class="w-8 h-8 text-black rotate-45" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m9 17 8 2L9 1 1 19l8-2Zm0 0V9"/>
+                    </svg>
+                    <p class="ps-4 text-sm font-normal">Password changed successfully. You can login now</p>
+                </div>
+              </transition>
+          </form>
+      </div>
+  </div>
+</template>
   
   <script>
-  import {reactive} from 'vue';
+  import {reactive, ref} from 'vue';
   import { useRoute } from 'vue-router';
   import axios from 'axios'
   export default {
@@ -23,11 +66,15 @@
             password_confirm: ''
         })
 
+        const submitSuccessfully = ref(false);
+
         const submit = async () => {
         try {
           await axios.post('/api/reset/', data);
-
-          console.log('Password reset email sent successfully!');
+          submitSuccessfully.value = true
+          setTimeout(() => {
+            submitSuccessfully.value = false;
+          }, 5000);
         } catch (error) {
           console.error('Error sending password reset email:', error.message);
         }
@@ -35,7 +82,8 @@
 
         return {
             data,
-            submit
+            submit,
+            submitSuccessfully
         }
     }
   }
